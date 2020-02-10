4e176e1d60ca9107b208dc1d860d93e4d22f19a4
Bernadette Davis <bsd231@nyu.edu>
2020-02-10 01:21:25 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 export default class ClickityClick extends Component {
   clickHandler = (event) => {
-    event.persist();
-    setTimeout(() => {
-      this.props.onDelayedClick(event);
-    }, this.props.delay);
+    //event.persist();
+    //setTimeout(() => {
+      //this.props.onDelayedClick(event);
+    //}, this.props.delay);
     
   }
   
   render() {
     return (
-      <div><button onClick={this.clickHandler} /></div>
+      //<div><button onClick={this.clickHandler} /></div>
       //    <DelayedButton onDelayedClick={ event => console.log(event) } delay={1500} />
 
     )
