e83269bb1fcf068b267feaf1199b76696a4d6f25
Bernadette Davis <bsd231@nyu.edu>
2020-02-10 01:33:18 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -5,8 +5,7 @@ export default class ClickityClick extends Component {
   
   render() {
     return (
-      //<div><button onClick={this.clickHandler} /></div>
-      //    <DelayedButton onDelayedClick={ event => console.log(event) } delay={1500} />
+      
 
     )
   }
