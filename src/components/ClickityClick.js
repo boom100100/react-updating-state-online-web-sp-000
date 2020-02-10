21eb9c71608b1a8f9aa9b306890d446b38a2de36
Bernadette Davis <bsd231@nyu.edu>
2020-02-10 01:39:05 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
-export default class ClickityClick extends Component {
-  
-  
+export default class ClickityClick extends React.Component {
+  constructor() {
+    super();
+ 
+    // Define the initial state:
+    this.state = {
+      hasBeenClicked: false
+    };
+  }
+ 
+  handleClick = () => {
+    // Update our state here...
+  };
+ 
   render() {
     return (
-      
-
-    )
+      <div>
+        <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
   }
-  
-  
 }
